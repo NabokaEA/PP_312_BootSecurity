985bd1a742ce211e5fab80f55f4bd9e0f59a2257
NabokaEA <nabokae@yandex.ru>
2022-09-14 21:28:40 +0300

    перенесены файлы из проекта 311

--- src/main/java/ru/nabokae/config/SuccessUserHandler.java
+++ src/main/java/ru/nabokae/config/SuccessUserHandler.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo.configs;
+package ru.nabokae.config;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -17,7 +17,7 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_USER")) {
-            httpServletResponse.sendRedirect("/user");
+            httpServletResponse.sendRedirect("/SpringBootCRUD/users/all");
         } else {
             httpServletResponse.sendRedirect("/");
         }
